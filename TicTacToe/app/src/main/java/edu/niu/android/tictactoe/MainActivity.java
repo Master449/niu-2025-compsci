9b7b319ff7a775567729b3f0ac2fa901c97cdd1d
David Flowers II <master4491@gmail.com>
2023-10-02 10:33:00 -0500

    v2 of TicTacToe.java game

--- TicTacToe/app/src/main/java/edu/niu/android/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/edu/niu/android/tictactoe/MainActivity.java
@@ -1,14 +1,78 @@
 package edu.niu.android.tictactoe;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.graphics.Point;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.GridLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+{
+    private Button [][] buttons;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        // setContentView(R.layout.activity_main);
+
+        buildUiByCode();
+    }
+
+    public void buildUiByCode()
+    {
+        // Get width of the screen
+
+        Point size = new Point();
+        getWindowManager().getDefaultDisplay().getSize(size);
+        int w = size.x / TicTacToe.SIDE;
+
+        // Create the layout manager as a GridLayout
+
+        GridLayout gridLayout = new GridLayout(this);
+        gridLayout.setColumnCount(TicTacToe.SIDE);
+        gridLayout.setRowCount(TicTacToe.SIDE);
+
+        // Create the buttons and add them to gridLayout
+
+        buttons = new Button [TicTacToe.SIDE][TicTacToe.SIDE];
+        ButtonHandler bh = new ButtonHandler();
+
+        for (int row = 0; row < TicTacToe.SIDE; row++)
+        {
+            for (int col = 0; col < TicTacToe.SIDE; col++)
+            {
+                buttons [row][col] = new Button(this);
+                buttons [row][col].setTextSize((int) (w * .2));
+                buttons [row][col].setOnClickListener(bh);
+                gridLayout.addView(buttons [row][col], w, w);
+            }
+        }
+
+        // Set gridLayout as the View of this Activity
+
+        setContentView(gridLayout);
+    }
+
+    public void update(int row, int col)
+    {
+        Log.w("MainActivity", "Inside update: " + row + ", " + col);
+        buttons [row][col].setText("X");
+    }
+
+    private class ButtonHandler implements View.OnClickListener
+    {
+        public void onClick(View v)
+        {
+            Log.w("MainActivity", "Inside onClick, v = " + v);
+
+            for (int row = 0; row < TicTacToe.SIDE; row++)
+                for (int column = 0; column < TicTacToe.SIDE; column++)
+                    if (v == buttons [row][column])
+                        update(row, column);
+        }
     }
 }
