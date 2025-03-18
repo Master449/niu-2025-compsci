8ea6cc7f91a97b053d49ec0a279f08a27b7ab438
Master449 <20436145+Master449@users.noreply.github.com>
2025-03-17 19:33:37 -0500

    Finished pseudocode for project4 (assuming im not mistaken on the wording for this assignment)

--- z1942130_project4_dir/z1942130_project4.cpp
+++ z1942130_project4_dir/z1942130_project4.cpp
@@ -58,23 +58,100 @@ int main(int argc, char *argv[]) {
 
         // Grab the next line, which will be the history of the process
         getline(input_file, process_stat_line);
-
         stringstream sl(process_stat_line);
 
+        // Get two at a time from the string stream
         while (sl >> type >> burst) {
+            // Check if we are at the end
             if (type == "N" && burst == "0") 
                 break;
-            char t = type[0];
-            int b = stoi(burst);
-            tmp_process.history.push_back(std::make_pair(t, b));
+            // Push them onto the history
+            tmp_process.history.push_back(std::make_pair(type[0], stoi(burst)));
         }
-
+        
+        // Put process onto the entry queue, and increment the process id
         entryq.push_back(tmp_process);
         process_id++;
     }
 
-    for (long unsigned int i = 0; i < entryq.size(); i++) { entryq[i].debug_info(); }
-
+    // Done reading and processing
     input_file.close();
+
+    for (auto it : entryq) {
+        cout << it.arrival_time << " ";
+    }
+
+    //
+    // Main Scheduling Loop
+    //
+    int timer = 0;
+    int total_proccess = 0;
+    bool active_flag, input_flag, output_flag;
+
+    // quick reference
+    // deque has methods
+         // empty
+         // front - first
+         // back - last
+         // size
+
+    do {
+        // Timekeeping
+        // loop through entryq
+            // If timer > arrival time && total processes < IN_USE
+                // push process onto the readyq
+        
+        // -------------- Active Process -------------------
+        // if (active_flag)
+            // it could end CPU burst to I or O
+            // it could end a CPU burst to terminate
+        // else if(readyq empty && entryq empty && total processes < IN_USE)
+            // increment idle time -- no active process
+        // else
+            // obtain from readyq
+            // if readyq empty && total processes < IN_USE
+                // if entryq not empty
+                    // obtain a process from the entryq
+                    // store tmp burst
+                    // update active-flag
+                    // print if moved to queue
+
+        // -------------- Input ------------------------
+        // If (input_flag)
+            // if tmp-i-burst is 0
+                // update history index
+                // move to ready queue
+                // print if moved to queue
+        // else
+            // Look for one in the input queue
+            // if found
+                // print its movement change
+                // tmp value to hold its burst
+
+        // -------------- Output ---------------
+        // If (output_flag)
+            // if tmp-o-burst is 0
+                // update history index
+                // move to ready queue
+                // print if moved to queue
+        // else
+            // Look for one in the output queue
+            // if found
+                // print its movement change
+                // tmp value to hold its burst
+
+        
+        // Timekeeping 2 electric boogaloo
+        // if timer % HOW_OFTEN == 0
+            // print ID of active
+            // print ID of input
+            // print ID of output
+            // contents of entryq
+            // contents of inputq
+            // contents of outputq
+        timer++;
+    } while (timer <= MAX_TIME);
+
+    
     return 0;
 }
