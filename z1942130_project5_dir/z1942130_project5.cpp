6d8f7df2095c9e7afe31e470f9e783167ace8bd3
Master449 <20436145+Master449@users.noreply.github.com>
2025-03-30 13:18:43 -0500

    Threads seem to be working, foundation is almost done it seems.

--- z1942130_project5_dir/z1942130_project5.cpp
+++ z1942130_project5_dir/z1942130_project5.cpp
@@ -17,20 +17,33 @@ Programmer: David Flowers II
 
 using std::string;
 using std::cout;
+using std::cerr;
 using std::endl;
 using std::stoi;
 
+string shared_data = "All work and no play makes Jack a dull boy.";
+
+sem_t rw_semaphore; // Semaphore for both readers and writers
+sem_t ro_semaphore; // Semaphore for only readers critical section
+
 void *writer(void *param) {
-    return;
+    long tid;
+    tid = (long)param;
+    cout<< "Hello World! It's me, thread " << tid << endl;
+    pthread_exit(NULL);
 }
 
 void *reader(void *param) {
-    return;
+    long tid;
+    tid = (long)param;
+    cout<< "Hello World! It's me, thread " << tid << endl;
+    pthread_exit(NULL);
 }
 
 int main(int argc, char *argv[]) {
-    
-    int reader_count, writer_count;
+
+    int reader_count, writer_count, rt, wt;
+    long total_threads;
 
     // Check command line args
     if (argc != 3) {
@@ -52,7 +65,36 @@ int main(int argc, char *argv[]) {
         exit(3);
     }
 
-    // Initialization of semaphores.
+    cout << "*** Reader-Writer Problem Simulation ***" << endl
+         << "Number of reader threads: " << reader_count << endl
+         << "Number of writer threads: " << writer_count << endl;
+
+    // Initialization of semaphores
+    if(sem_init(&rw_semaphore, 0, 1) != 0 || sem_init(&ro_semaphore, 0, 1) != 0) {
+        cerr << "Semaphore init failed" << endl;
+        exit(4);
+    }
+    
+    pthread_t reader_threads[reader_count];
+    pthread_t writer_threads[writer_count];
+    
+    // Initialize reader threads
+    for(int i=0; i<reader_count; i++) {
+       rt = pthread_create(&reader_threads[i], NULL, reader, (void *)i);
+       if (rt){
+          cerr << "Failed to init. reader thread" << endl;
+          exit(5);
+       }
+    }
+
+    // Initialize writer threads
+    for(int i=0; i<writer_count; i++) {
+       wt = pthread_create(&writer_threads[i], NULL, writer, (void *)i);
+       if (wt){
+          cerr << "Failed to init. writer thread" << endl;
+          exit(6);
+       }
+    }
     // Create reader and writer threads.
     // Wait for reader threads to finish.
     // Wait for writer threads to finish.
